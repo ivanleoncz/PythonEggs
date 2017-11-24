0af92d99d40e477c8d33bf865c36140f49f097aa
ivanlmj <ivanlmj@gmail.com>
2017-11-23 22:39:48 -0600

    adding if/elif depending on UA string

--- 3.4/flask_user_agent.py
+++ 3.4/flask_user_agent.py
@@ -7,7 +7,22 @@ app = Flask(__name__)
 @app.route("/")
 def f_index():
     ua = request.headers.get('User-Agent')
-    return "<h2> User-Agent: </h2> %s" % ua
+    if "Windows Phone" in ua:
+        return "Hello, Windows Phone!"
+    elif "Android" in ua:
+        return "Hello, Android!"
+    elif "iPad" in ua:
+        return "Hello, iPad!"
+    elif "iPhone" in ua:
+        return "Hello, iPhone!"
+    elif "Windows NT" in ua:
+        return "Hello, Windows!"
+    elif "X11" in ua:
+        return "Hello, GNU/Linux!"
+    elif "Mac OS X":
+        return "Hello, Macintosh!"
+    else:
+        return "I don't know you..."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
