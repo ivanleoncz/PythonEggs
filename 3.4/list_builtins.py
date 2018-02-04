ad5999eb3a085cc6a02caf9320026b2714c21ad4
ivanlmj <ivanlmj@gmail.com>
2018-02-04 16:34:20 -0600

    improving formatting and messages

--- 3.4/list_builtins.py
+++ 3.4/list_builtins.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-''' Demonstrating some built-ins for list data types.'''
-
+''' Demonstrating some built-ins for list data type.'''
 
 l = [3,5,2,0,4,5,7,3,1,6,23,4,9,2,4]
 
-print("\nList:",l)
+print("\nLIST:",l)
 l.append(25)
-print("\nList (appended number 25):",l)
+print("\n- appending number 25:",l)
 l.pop(0)
-print("List (index 0 removed):",l)
+print("- removing value from index 0:",l)
 l.sort()
-print("List (sorted):",l)
+print("- sorting list:",l)
 l.reverse()
-print("List (reversed):",l)
-print("List (returns first index ocurrence of value 3):",l.index(3))
-print("List (count ocurrences of value 4):",l.count(4))
+print("- reversing list:",l)
+print("- returning first index ocurrence of value 3:",l.index(3))
+print("- count ocurrences of value 4:",l.count(4))
 
