a753258c2dbd49692be75c93f239a34928662b4f
ivanlmj <ivanlmj@gmail.com>
2018-02-04 16:31:33 -0600

    demonstrates built-in functions (string data type)

---
+++ 3.4/string_builtins.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+words = "The Words Of An Eternal Soul Will Remain Forever In Our Hearts."
+
+print("\nSTRING:",words)
+print("\n- numbers of ocurrences of the letter w:",words.count("W"))
+print("- print all lowercase:",words.lower())
+print("- print all uppercase:",words.upper())
+print("- split into substrings:",words.split())
+print("- returns index for the first ocurrence of the letter a:",words.find("a"))
+print("- length of string:",len(words))
