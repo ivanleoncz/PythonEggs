e16ec4ac6364430fdd8c4e50e3da7076b2ab36e3
ivanlmj <ivanlmj@gmail.com>
2018-02-10 12:30:50 -0600

    improving/simplifying script structure and execution
    - adding script docstring
    - adjusting variable names
    - renaming database file
    - removing unnecessary functions
    - adding DROP TABLE IF EXISTS to create_table() function
    - removing unnecessary if/else structure

--- 3.4/sqlite_database_builder/sqlite3db.py
+++ 3.4/sqlite_database_builder/sqlite3db.py
@@ -1,55 +1,30 @@
 #!/usr/bin/python3
+''' Initializing SQLite3 Database. '''
 
 import sqlite3
 
-db = sqlite3.connect('sample_db.lite')
-cursor = db.cursor()
-table = "users"
-user = "Dennis Ritchie"
-email = "dennis@unix.com"
-country = "USA"
+db       = sqlite3.connect('app_users.lite')
+cursor   = db.cursor()
+table    = "users"
 
-def check_table():
-    query = "SELECT name FROM sqlite_master WHERE type = 'table' and name = '{0}';".format(table)
-    cursor.execute(query)
-    db.commit()
-    check = cursor.fetchall()
-    return check
+name     = "Dennis Ritchie"
+country  = "USA"
+email    = "dennis@unix.com"
+password = "us3rp4ss"
 
 def create_table():
-    query = "CREATE TABLE {0} (id INTEGER PRIMARY KEY, name TEXT, email TEXT unique, country TEXT);".format(table)
-    cursor.execute(query)
+    drop = "DROP TABLE IF EXISTS {0}".format(table)
+    cursor.execute(drop)
+    create = "CREATE TABLE {0} (id INTEGER PRIMARY KEY, name TEXT, country TEXT, email TEXT UNIQUE, password TEXT);".format(table)
+    cursor.execute(create)
     db.commit()
     return None
 
 def create_user():
-    query = "INSERT INTO {0} (id,name,email,country) VALUES(1,'{1}','{2}','{3}');".format(table,user,email,country)
-    cursor.execute(query)
+    insert = "INSERT INTO {0} (id,name,country,email,password) VALUES(1,'{1}','{2}','{3}','{4}');".format(table,name,country,email,password)
+    cursor.execute(insert)
+    db.commit()
     return None
 
-def find_user():
-    select = "SELECT name FROM {0} WHERE name = '{1}';".format(table,user)
-    cursor.execute(select)
-    db.commit()
-    data = cursor.fetchall()
-    return data
-
-
-print("Checking existence of table: {0}").format(table)
-check = check_table()
-if len(check) == 0:
-    print("Creating table: {0}").format(table)
-    create_table()
-    check = check_table()
-    if len(check) != 0:
-        print("Creating User: {0}").format(user)
-        create_user()
-        data = find_user()
-        if len(data) != 0:
-            print("-> User successfully created!\n")
-        else:
-            print("User not found...")
-    else:
-        print("Table not found...")
-else:
-    print("Table {0} already exists.".format(table))
+create_table()
+create_user()
