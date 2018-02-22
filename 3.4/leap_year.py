17ab7a0c3bb0aeadaa1fb77a1af82d44b86faf61
ivanlmj <ivanlmj@gmail.com>
2018-02-21 21:32:22 -0600

    improving outputs + fixing leap year evaluation

--- 3.4/leap_year.py
+++ 3.4/leap_year.py
@@ -3,20 +3,16 @@
 def is_leap(year):
     leap = False
     if year % 4 == 0:
+        leap = True
         if year % 100 == 0:
+            leap = False
             if year % 400 == 0:
                 leap = True
-                return leap
-            else:
-                return leap
-        else:
-            return leap
-    else:
-        return leap
+    return leap
 
-year = int(input("\nInsert a year, starting from 1990: "))
+year = int(input("\nInsert a year, starting from 1900: "))
 
-if year >= 1990:
+if year >= 1900:
     print(is_leap(year))
 else:
-    print("Year must start from 1990.")
+    print("Year must start from 1900.")
