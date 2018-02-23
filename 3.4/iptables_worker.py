8e213f0946d2df8dad82a8793d192fe351c567d8
ivanlmj <ivanlmj@gmail.com>
2018-02-22 18:15:16 -0600

    refactoring iptables_worker
    * adding module docstring + other comments
    * adding module variables (__author__,__version__)
    * dividing options into module functions
    * separating user interction for script execution

--- 3.4/iptables_worker.py
+++ 3.4/iptables_worker.py
@@ -1,54 +1,69 @@
 #!/usr/bin/python3
 
+""" Demonstration of Firewall rules management for IPTABLES/Netfilter. """
+
+__version__ = "1.0"
+__author__  = "ivanleoncz"
+
+
 import os
 import subprocess as sp
 import sys
 
-if os.getuid() != 0:
-    print("Must be executed by root user!")
-    sys.exit(2)
 
-if not os.path.isfile('/sbin/iptables'):
-    print("No iptables binary was found!")
-    sys.exit(2)
-try:
-    loop = 0
-    while loop == 0:
+def worker_add():
+    r = sp.call(['/sbin/iptables',
+                 '-A','INPUT','-i','lo','-s','127.0.0.1',
+                 '-p','tcp','--dport','10800','-j','DROP'])
+    if r == 0:
+        return "Successful!\n"
+    else:
+        return "Fail!\n"
+
+
+def worker_del():
+    r = sp.call(['/sbin/iptables',
+                 '-D','INPUT','-i','lo','-s','127.0.0.1',
+                 '-p','tcp','--dport','10800','-j','DROP'])
+    if r == 0:
+        return "Successful!\n"
+    else:
+        return "Fail!\n"
+
+
+def worker_list():
+    p = sp.Popen('/sbin/iptables -nvL | grep 10800', shell=True, stdout=sp.PIPE)
+    output = p.stdout.read()
+    if len(output) == 0:
+        return "Not Found!\n"
+    else:
+        return output.decode('utf-8')
+
+
+if __name__ == "__main__":
+    # the script can only be executed by root
+    if os.getuid() != 0:
+        print("Must be executed by root user!")
+        sys.exit(2)
+    # iptables binary must be present
+    if not os.path.isfile('/sbin/iptables'):
+        print("No iptables binary was found!")
+        sys.exit(2)
+    while True:
         sp.call(['clear'])
-        print("<Iptables Worker>\n")
-        print("1. Add\n2. Del\n3. List\n4. Exit\n\n")
-        opt = input("Option: ")
+        print("\n[Iptables Worker]")
+        opt = input("\n1. Add\n2. Del\n3. List\n4. Exit\n\nOption: ")
         if opt == "1":
-            print("Addind...\n")
-            r = sp.call(['/sbin/iptables','-A','INPUT','-i','lo','-s','127.0.0.1','-p','tcp','--dport','10800','-j','DROP'])
-            if r == 0:
-                print("Successful!")
-            else:
-                print("Fail!")
+            print(worker_add())
             input("Press any key...\n")
         elif opt == "2":
-            print("Deleting...\n")
-            r = sp.call(['/sbin/iptables','-D','INPUT','-i','lo','-s','127.0.0.1','-p','tcp','--dport','10800','-j','DROP'])
-            if r == 0:
-                print("Successful!")
-            else:
-                print("Fail!")
+            print(worker_del())
             input("Press any key...\n")
-        elif opt == "3":    
-            print("Listing...\n")
-            p = sp.Popen('/sbin/iptables -nvL | grep 10800', shell=True, stdout=sp.PIPE)
-            output = p.stdout.read()
-            if len(output) == 0:
-                print("Not Found!\n")
-            else:
-                print(output.decode('utf-8'))
+        elif opt == "3":
+            print(worker_list())
             input("Press any key...\n")
         elif opt == "4":
-            loop = 1
-            print("\nBye!\n")
+            input("Bye!\n")
+            break
         else:
-            print("Wrong Option!\n")
-            input("Press any key...\n")
-except KeyboardInterrupt:
-    print("\nInterrupted!\n")
-
+            input("Wrong Option! Press any key...\n")
