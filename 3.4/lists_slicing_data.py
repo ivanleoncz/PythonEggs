66c2c08f9f30f3d067bfd54eb8e95af9c5dde864
ivanlmj <ivanlmj@gmail.com>
2018-03-21 12:56:55 -0600

    adding example for lists (slicing)

---
+++ 3.4/lists_slicing_data.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Demonstrating slicing. """
+
+l = ["p","y","t","h","o","n"]
+print("\nList:",l)
+print("\nSlices:")
+print("* first index:                              ",l[0])
+print("* from index 0 to index 1:                  ",l[:2])
+print("* from index 2 to end of list:              ",l[2:])
+print("* from index 1 to penultimate index:        ",l[1:-1])
+print("* from index 0 to enf of list (incr. by 2): ",l[::2])
+print("* everything, except last two items:        ",l[:-2])
+print("* only the last two items:                  ",l[-2:])
+print("* last index:                               ",l[-1])
