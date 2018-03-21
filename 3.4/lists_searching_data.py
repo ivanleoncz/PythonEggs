4a5cda36820304eb5bc87a32c2cc018f7f11ed7b
ivanlmj <ivanlmj@gmail.com>
2018-03-21 11:01:01 -0600

    adding example of lists (getting index data)

---
+++ 3.4/lists_searching_data.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Searching data on lists. """
+
+l = ["Python","C++","Java","Cobol","Scala"]
+iprint("List:",l)
+
+# getting indexes from list data
+idx1 = l.index("C++")
+idx2 = l.index("Scala")
+print("Index for C++:   ", idx1)
+print("Index for Scala: ", idx2)
+
