210d5fd18b42ab44f77f41bc6dd47561ae2edc6c
ivanlmj <ivanlmj@gmail.com>
2018-03-21 18:28:10 -0600

    adding print list length

--- 3.4/lists_adding_data.py
+++ 3.4/lists_adding_data.py
@@ -10,12 +10,15 @@ l.append(3.4)
 l.append({"Author":"Guido van Rossum"})
 l.append(["Computer Science","Web Development","Geoprocessing"])
 print(l)
+print("* Length:",len(l),"\n")
 
 # inserting data at index 2
 l.insert(2,1991)
 print(l)
+print("* Length:",len(l),"\n")
 
 # concatenating lists
 l.extend([{"Age":27},"Python Software Foundation"])
 print(l)
+print("* Length:",len(l),"\n")
 
