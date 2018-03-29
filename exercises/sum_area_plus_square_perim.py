b7cf66abc848cf39d9ef5bac19806f42bdd1be57
ivanlmj <ivanlmj@gmail.com>
2018-03-28 22:51:12 -0600

    adding example for polygon calculation

---
+++ exercises/sum_area_plus_square_perim.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Sums the area and the square of the perimeter of a regular polygon. """
+
+from math import pi,tan
+
+__author__ = "@ivanleoncz"
+
+def polysum(n,s):
+    """ n: number of sides of the polygon
+        s: length of each side
+
+        returns: area + (square of the perimeter)
+    """
+    perim = 0
+    # increments side length by the number of sides
+    for side in range(n):
+        perim += s
+    area = 0.25*n*(s**2) / tan(pi/n)
+    result = area + perim ** 2
+    return round(result, 4)
+
+print(polysum(n,s))
