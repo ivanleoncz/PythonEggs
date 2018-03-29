26000bd59e5d9087ee6b14ceaa5614d20b620956
ivanlmj <ivanlmj@gmail.com>
2018-03-29 00:30:09 -0600

    applying fixies
    - improving docstring
    - adding input for range of numbers
    - removing / adding comments
    - improving output

--- while_sum_all_numbers.py
+++ while_sum_all_numbers.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
+""" Sums all positive integers contained in a range of numbers. 
 
-total = 0
-n = 1
-while n <= 6:
-    # total + the current iterator, starting by ZERO
-    #  0 + 1 = 1
-    #  1 + 2 = 3
-    #  3 + 3 = 6
-    #  6 + 4 = 10
-    # 10 + 5 = 15
-    # 15 + 6 = 21
-    total += n     # 1  # 3  # 6  # 10  # 15  # 21
-    n += 1         # 2  # 3  # 4  # 5   # 6   # 7
-print(total)
+Notice: only while loop is allowed for solving the problem.
+"""
+
+limit  = int(input("\nInsert a positive integer, please: "))
+total  = 0
+number = 1
+while number <= limit:
+    total  += number  # increments by the current number
+    number += 1       # updates the number for while condition
+
+print("\nSum of all numbers between 1 and {0}: {1}".format(limit,total))
