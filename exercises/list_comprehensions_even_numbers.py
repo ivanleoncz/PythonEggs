05b98cdcb47620f95793deff336da13a71b31b99
ivanlmj <ivanlmj@gmail.com>
2018-04-05 21:23:24 -0500

    adding list comprehension exercise

---
+++ exercises/list_comprehensions_even_numbers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Maps even numbers from a list of numbers. """
+
+__author__ = "@ivanleoncz"
+
+
+numbers = [0, 3, 4, 11, 19, 38, 62, 176, 293, 306]
+
+print("All Numbers:", numbers)
+print("Even Numbers:", [n for n in numbers if n % 2 == 0])
+
