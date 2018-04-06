9a6eef7047e9f3dde045a92a65f64c5cef2f295d
ivanlmj <ivanlmj@gmail.com>
2018-04-06 18:59:30 -0500

    adding directory and modules

---
+++ tricks/join_list_to_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+""" Transforms a list into a string. """
+
+l = ["I", "am", "the", "law"]
+print(" ".join(l))
---
+++ tricks/join_print_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3 
+""" Printing all elements from a list, using join. """
+
+__author__ = "@ivanleoncz"
+
+l = ["John", "love", "is", "all", "you", "need"]
+print("\n".join(l))
