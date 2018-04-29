45c031b9230c33ff39133e40c09e235f533ed68e
ivanlmj <ivanlmj@gmail.com>
2018-04-29 16:28:04 -0500

    refactoring code structure

--- utils/sqlite3_db_check.py
+++ utils/sqlite3_db_check.py
@@ -4,18 +4,13 @@
 __author__ = "@ivanleoncz"
 
 import os
-import sys
 
-
-def sqlite3_check():
-    db_filename = input("Database filename (end with .sqlite3):")
+def sqlite3_check(db_filename):
     if os.path.isfile(db_filename) and os.path.getsize(db_filename) > 100:
         with open(db_filename,'r', encoding = "ISO-8859-1") as f:
             header = f.read(100)
             if header.startswith('SQLite format 3'):
-                return "INFO: database file has been detected."
+                return True
             else:
-                return "ERROR: NO database file has been found."
+                return False
 
-
-print(sqlite3_check())
