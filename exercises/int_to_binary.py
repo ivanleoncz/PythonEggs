f2a783bbee2e27a0dca094a207583ad17e5fe252
ivanlmj <ivanlmj@gmail.com>
2018-05-15 19:06:06 -0500

    moving solution to recursion model

--- exercises/int_to_binary.py
+++ exercises/int_to_binary.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
+""" Representing integer numbers as binary numbers. """
+
+__author__ = "@ivanleoncz"
+
+
+def int_to_bin(dividend):
+    # if new dividend is equal to zero, stop calculation
+    if dividend != 0:
+        # calcularing remainder based on the current dividend
+        remainder = dividend % 2
+        # appending remainder (0 || 1) to list
+        binary.append(str(remainder))
+        # calculating new dividend for next recursion step
+        dividend = dividend // 2
+        # performing recursion with a new dividend
+        return int_to_bin(dividend)
+
 
 binary = []
-
-n = int(input("\nPlease, insert an INT number: "))
-
-dividend = n
-while dividend > 0:
-    remainder = dividend % 2
-    binary.append(str(remainder))
-    dividend  = dividend // 2
-
+n = int(input("\nInteger: "))
+int_to_bin(n)
 binary.reverse()
-print("Binary:","".join(binary))
+print("Binary: ","".join(binary))
+
