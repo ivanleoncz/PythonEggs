968c974482e992f35b3097a5267f82c4a732be2b
ivanlmj <ivanlmj@gmail.com>
2018-08-22 01:17:09 -0500

    adding solution for playGame function

--- exercises/edX/pset_04/ps4a.py
+++ exercises/edX/pset_04/ps4a.py
@@ -297,11 +297,24 @@ def playGame(wordList):
  
     2) When done playing the hand, repeat from step 1    
     """
-    # TO DO ... <-- Remove this comment when you code this function
-    print("playGame not yet implemented.") # <-- Remove this line when you code the function
+    play = 0
+    hand = None
+    while True:
+        opt = input("Enter n to deal a new hand, r to replay the last hand, or e to end game: ")
+        if opt == "n":
+            hand = dealHand(HAND_SIZE)
+            playHand(hand, wordList, HAND_SIZE)
+            play += 1
+        elif opt == "r":
+            if play == 0:
+                print("You have not played a hand yet. Please play a new hand first!")
+            else:
+                playHand(hand, wordList, HAND_SIZE)
+        elif opt == "e":
+            break
+        else:
+            print("Invalid command.")
    
-
-
 
 #
 # Build data structures used for entire session and play game
