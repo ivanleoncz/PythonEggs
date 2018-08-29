1bc7d51df1ca927c79845bc3bb4fa7d10532d9c8
ivanlmj <ivanlmj@gmail.com>
2018-08-29 10:56:56 -0500

    addin upper case cypher + improving variable names

--- exercises/caesar_cypher.py
+++ exercises/caesar_cypher.py
@@ -2,27 +2,32 @@
 import string
 from json import dumps
 
-alphabet = list(string.ascii_lowercase)
-alphabet_caesar = alphabet[:]
+lower = list(string.ascii_lowercase)
+lower_caesar = lower[:]
+upper = list(string.ascii_uppercase)
+upper_caesar = upper[:]
+
 rotate = int(input("Number of rotations: "))
 word = input("Word to encrypt: ")
 
 caesar_map = {}
 
 for i in range(rotate):
-#    move_beggining = alphabet_caesar.pop(-1)
-#    alphabet_caesar.insert(0, move_beggining)
-    move_end = alphabet_caesar.pop(0)
-    alphabet_caesar.append(move_end)
+    move_end = lower_caesar.pop(0)
+    lower_caesar.append(move_end)
+    move_end = upper_caesar.pop(0)
+    upper_caesar.append(move_end)
 
-for k, v in zip(alphabet, alphabet_caesar):
+for k, v in zip(lower, lower_caesar):
+    caesar_map[k] = v
+
+for k, v in zip(upper, upper_caesar):
     caesar_map[k] = v
 
 caesar_word = ''
 for letter in word:
     caesar_word += caesar_map[letter]
     
-print("Alphabet:\n{0}".format(alphabet))
-print("Alphabet Caesar (rotate {0}):\n{1})".format(rotate, alphabet_caesar))
+
 print("Caesar Map:\n{0}".format(dumps(caesar_map, indent=4)))
 print("Encrypted Word:\n{0}".format(caesar_word))
