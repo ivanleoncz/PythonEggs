cc3e21a7e47e54de4c1530d867e73989a1fe19ff
ivanlmj <ivanlmj@gmail.com>
2018-11-03 17:48:15 -0600

    adding Delete and Delete All Records functions

--- databases/mongodb_crud.py
+++ databases/mongodb_crud.py
@@ -35,11 +35,14 @@ class Connector(object):
         db = self.client.blog.Users
         data = db.find_one({"Name":name})
         self.client.close()
-        print("\n - ID:", data['_id'])
-        print(" - Name:", data['Name'])
-        print(" - Role:", data['Role'])
-        print(" - CreateTime:", data['CreateTime'])
-        print(" - UpdateTime:", data['UpdateTime'])
+        if data is not None:
+            print("\n - ID:", data['_id'])
+            print(" - Name:", data['Name'])
+            print(" - Role:", data['Role'])
+            print(" - CreateTime:", data['CreateTime'])
+            print(" - UpdateTime:", data['UpdateTime'])
+        else:
+            print("No Record Was Found!")
 
 
     def read_all_records(self, dump=False):
@@ -54,30 +57,55 @@ class Connector(object):
             print(dumps(data, indent=2, default=json_util.default))
 
 
+    def delete_record(self):
+        name = input("\nName: ")
+        db = self.client.blog.Users
+        data = db.remove({"Name": name})
+        self.client.close()
+        print(data)
+
+
+    def delete_all_records(self):
+        opt = input("\nDelete All Records (y/n)?")
+        if opt == "y":
+            db = self.client.blog.Users
+            data = db.remove({})
+            self.client.close()
+            print("Done!")
+        elif opt == "n":
+            print("Aborted!")
+
+
 database = Connector()
 
 while True:
     print("\n\n=====> [MongoDB CRUD] <=====\n")
     options = [
-                ["Create","c"],
-                ["Read","r"],
-                ["Update","u"],
-                ["Delete","d"],
-                ["Read All","a"],
-                ["Exit","e"]
+                [1, "Create"],
+                [2, "Read"],
+                [3, "Update"],
+                [4, "Delete"],
+                [5, "Read All"],
+                [6, "Delete ALl"],
+                [0, "Exit"]
               ]
     for l in options:
         print(" - {0} ({1})".format(l[0],l[1]))
-    opt = input("\n")
-    if opt == "c":
+
+    opt = int(input("\n"))
+    if opt == 1:
         print(database.create_record())
-    elif opt == "r":
+    elif opt == 2:
         database.read_record()
-    elif opt == "a":
+    elif opt == 4:
+        database.delete_record()
+    elif opt == 5:
         j = input("As JSON (y/n)?")
         if j == "y":
             database.read_all_records(True)
         else:
             database.read_all_records()
-    elif opt == "e":
+    elif opt == 6:
+        database.delete_all_records()
+    elif opt == 0:
         sys.exit(0)
