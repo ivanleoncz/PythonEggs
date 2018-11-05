ee7b3a0345142df75749045148466a6e098b4da3
ivanlmj <ivanlmj@gmail.com>
2018-11-05 09:42:58 -0600

    adding exercise (numbers as list and tuple)

---
+++ exercises/ex4_numbers_and_comma_as_list_tuple.py
@@ -0,0 +1,14 @@
+""" Write a program which accepts a sequence of comma-separated numbers
+from console and generate a list and a tuple which contains every number.
+
+Suppose the following input is supplied to the program:
+34, 67, 55, 33, 12, 98
+
+Then, the output should be:
+['34', '67', '55', '33', '12', '98']
+('34', '67', '55', '33', '12', '98')
+"""
+numbers = input("\nInsert a list o numbers, separated by comma, please: ")
+
+print(numbers.split(','))
+print(tuple(numbers.split(',')))
