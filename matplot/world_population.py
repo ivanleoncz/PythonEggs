13c91f868417eb4f729a0069fc4e65fe5e80eae1
ivanlmj <ivanlmj@gmail.com>
2019-01-21 22:52:00 -0600

    creating simple graphic - world population growth

---
+++ matplot/world_population.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+# axis
+#
+# year: horizontal
+years = [1950, 1970, 1990, 2010]
+# population: vertical
+population = [2.519, 3.692, 5.263, 6.972]
+
+# building graphic
+plt.plot(years, population)
+
+# show graphic
+
+plt.show()
