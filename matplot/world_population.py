cc32329ed3d5e014a7f48ab6bac634bccce7a067
ivanlmj <ivanlmj@gmail.com>
2019-01-21 23:09:51 -0600

    adding condition for plot type

--- matplot/world_population.py
+++ matplot/world_population.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 
+print("\nWorld Population Graphic\n")
+
 # axis
 #
 # year: horizontal
@@ -7,9 +9,14 @@ years = [1950, 1970, 1990, 2010]
 # population: vertical
 population = [2.519, 3.692, 5.263, 6.972]
 
-# building graphic
-plt.plot(years, population)
+# building graphic, depending on user input
+opt = input("Lines or Scatter (l/s)? ")
+if opt == "l":
+    plt.plot(years, population)
+elif opt == "s":
+    plt.scatter(years, population)
+else:
+    print("Wrong option!")
 
 # show graphic
-
 plt.show()
