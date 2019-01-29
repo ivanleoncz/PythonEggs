136e07a9a602ebd544dbbb1c106611b8a8850d42
ivanlmj <ivanlmj@gmail.com>
2019-01-29 00:26:05 -0600

    adding grid for scatter and linear graphics

--- matplot/world_population.py
+++ matplot/world_population.py
@@ -5,16 +5,20 @@ print("\nWorld Population Graphic\n")
 # axis
 #
 # year: horizontal
-years = [1950, 1970, 1990, 2010]
+years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
 # population: vertical
-population = [2.519, 3.692, 5.263, 6.972]
+population = [2.584, 3.033, 3.700, 4.458, 5.331, 6.145, 6.958]
 
 # building graphic, depending on user input
-opt = input("Lines or Scatter (l/s)? ")
+opt = input("Lines / Scatter / Histogram (l/s/h)? ")
 if opt == "l":
     plt.plot(years, population)
+    plt.grid(True)
 elif opt == "s":
     plt.scatter(years, population)
+    plt.grid(True)
+elif opt == "h":
+    plt.hist(population, bins=5)
 else:
     print("Wrong option!")
 
