0a058b3beb38c44bb25f36c3a47383b79b10606a
ivanlmj <ivanlmj@gmail.com>
2019-04-01 16:44:47 -0600

    defining docstrings for the whole module

--- systemd/pysd.py
+++ systemd/pysd.py
@@ -1,12 +1,24 @@
+""" Python Binding for Systemd, uses the output from native systemctl commands,
+providing a programmable way of interacting with services, listing, verifying,
+starting, stopping, enabling and disabling services.
+"""
 import json
 import re
 import subprocess
 
 
 class Systemd():
-    """ Checks the status of a Systemd Unit (service) """
+    """ Main class for Systemd tasks. """
 
     def active_services(self, as_json=False):
+        """ Verifies all activated services.
+
+            as_json : boolean
+                Activates JSON format for returning data.
+
+            Returns:
+                List of active services.
+        """
         assert as_json is True or as_json is False, 'as_json => False or True'
         cmd = "systemctl list-units --type=service | grep 'active running'"
         output = subprocess.check_output(cmd, shell=True)
@@ -25,6 +37,14 @@ class Systemd():
         
 
     def enabled_services(self, as_json=False):
+        """ Verifies all enabled services.
+
+            as_json : boolean
+                Activates JSON format for returning data.
+
+            Returns:
+                List of enabled services.
+        """
         assert as_json is True or as_json is False, 'as_json => False or True'
         cmd = "systemctl list-unit-files --type=service | grep enabled"
         output = subprocess.check_output(cmd, shell=True)
@@ -39,6 +59,14 @@ class Systemd():
 
 
     def is_enabled(self, service=None):
+        """ Verifies if a service is enabled.
+
+            service : string
+                Service described as Systemd model (ex.: cron.service).
+
+            Returns:
+                True or False.
+        """
         assert service != None, 'must provide a valid service (ex. cron.service)'
         cmd = "systemctl status {} | grep enabled".format(service)
         output = subprocess.check_output(cmd, shell=True)
@@ -49,6 +77,14 @@ class Systemd():
 
 
     def is_active(self, service=None):
+        """ Verifies if a service is active.
+
+            service : string
+                Service described as Systemd model (ex.: cron.service).
+
+            Returns:
+                True or False.
+        """
         assert service != None, 'must provide a valid service (ex. cron.service)'
         cmd = "systemctl status {} | grep active".format(service)
         output = subprocess.check_output(cmd, shell=True)
