e4f42abae90f4922a645130e0414b9cc09351818
ivanleoncz <ivanlmj@gmail.com>
2023-11-23 20:46:14 -0600

    feat: no need of while for socket reuse
    A socket created is reused after every client connection.

--- sockets/server.py
+++ sockets/server.py
@@ -6,30 +6,31 @@ PORT = 60333
 BUFFER_SIZE = 32
 
 try:
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((HOST, PORT))
+    server.listen(5)
+    print(f"[INFO]: listening on {HOST}:{PORT}")
+
     while True:
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server.bind((HOST, PORT))
-        server.listen(5)
-        print(f"[INFO]: listening on {HOST}:{PORT}")
+
+        connection, addr = server.accept()
+        print(f"[INFO]: connection from {addr}")
 
         while True:
+            data = connection.recv(BUFFER_SIZE).decode("utf-8")
+            if data:
+                print(f"[INFO]: data received : ", data)
+                connection.send("server: message received!".encode("utf-8"))
+                if data in ('terminate', 'bye', ):
+                    print("[INFO]: connection terminated by client!")
+                    break
+                else:
+                    print("[INFO]: waiting for more messages...")
 
-            connection, addr = server.accept()
-            print(f"[INFO]: connection from {addr}")
+        connection.close()
 
-            while True:
-                data = connection.recv(BUFFER_SIZE).decode("utf-8")
-                if data:
-                    print(f"[INFO]: data received : ", data)
-                    connection.send("server: message received!".encode("utf-8"))
-                    if data in ('terminate', 'bye', ):
-                        print("[INFO]: connection terminated by client!")
-                        break
-                    else:
-                        print("[INFO]: waiting for more messages...")
 
-            connection.close()
-            break
 except KeyboardInterrupt:
     print("[INFO] server shutdown!")
